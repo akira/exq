9eb7a89c44ae0e47b0140b43f12d73276981d34c
Alex Kira <alex.kira@gmail.com>
2013-08-22 16:18:10 -0700

    Added Worker genserver to parse JSON and dispatch work.

--- lib/exq/worker.ex
+++ lib/exq/worker.ex
@@ -1,3 +1,48 @@
 defmodule Exq.Worker do 
   use GenServer.Behaviour
+
+  defrecord State, [:manager, :job]
+
+  def start(manager, job) do 
+    :gen_server.start(__MODULE__, {manager, job}, [])
+  end
+
+  def work(pid) do 
+    :gen_server.cast(pid, :work)
+  end
+
+##===========================================================
+## gen server callbacks
+##===========================================================
+
+  def init({manager, job}) do 
+    {:ok, State.new(manager: manager, job: job)}
+  end
+
+  def handle_cast(:work, state) do
+    job_dict = JSEX.decode!(state.job)
+    handler_module = Dict.get(job_dict, "class")
+    handler_args = Dict.get(job_dict, "args")
+    dispatch_work(handler_module, :perform, handler_args)
+    {:stop, :normal, state}
+  end
+
+  def code_change(_old_version, state, _extra) do
+    {:ok, state}
+  end
+
+  def terminate(_reason, _state) do 
+    :ok
+  end
+
+##===========================================================
+## Internal Functions
+##===========================================================
+ 
+  def dispatch_work(worker_module, args) do 
+    dispatch_work(worker_module, :perform, args)
+  end
+  def dispatch_work(worker_module, method, args) do 
+    :erlang.apply(binary_to_atom("Elixir.#{worker_module}"), method, args)
+  end
 end
---
+++ test/worker_test.exs
@@ -0,0 +1,53 @@
+Code.require_file "test_helper.exs", __DIR__
+
+defmodule WorkerTest do
+  use ExUnit.Case
+
+  def perform do
+    IO.puts("PERFORM") 
+  end
+  def custom_perform do
+    IO.puts("CUSTOM_PERFORM") 
+  end
+  
+  def assert_terminate(worker, is_normal) do 
+    :erlang.monitor(:process, worker)
+    Exq.Worker.work(worker)
+    receive do 
+      {:'DOWN', ref, _, _pid, :normal} -> assert is_normal
+      {:'DOWN', ref, _, _pid, _} -> assert !is_normal
+    after_timeout ->
+      assert !is_normal
+    end 
+  end
+ 
+  test "execute valid job with perform" do
+    {:ok, worker} = Exq.Worker.start(self, 
+      "{ \"queue\": \"default\", \"class\": \"WorkerTest\", \"args\": [] }")
+    assert_terminate(worker, true)
+  end
+  
+  test "execute valid job with custom function" do
+    {:ok, worker} = Exq.Worker.start(self, 
+      "{ \"queue\": \"default\", \"class\": \"WorkerTest/custom_perform\", \"args\": [] }")
+    assert_terminate(worker, true)
+  end
+  
+  test "execute job with invalid JSON" do
+    {:ok, worker} = Exq.Worker.start(self, 
+      "{ invalid: json: this: is}")
+    assert_terminate(worker, false)
+  end
+  
+  test "execute invalid module perform" do
+    {:ok, worker} = Exq.Worker.start(self, 
+      "{ \"queue\": \"default\", \"class\": \"NonExistant\", \"args\": [] }")
+    assert_terminate(worker, false)
+  end
+  
+  test "execute invalid module function" do
+    {:ok, worker} = Exq.Worker.start(self,
+      "{ \"queue\": \"default\", \"class\": \"WorkerTest/nonexist\", \"args\": [] }")
+    assert_terminate(worker, false)
+  end
+end
