e0ec00d20acca5147df490bca036a865a390a76a
Justin McNally <justin@kohactive.com>
2014-10-25 04:51:58 -0500

    fix charlist host handling

--- lib/mix/tasks/exq.ui.ex
+++ lib/mix/tasks/exq.ui.ex
@@ -10,7 +10,7 @@ defmodule Mix.Tasks.Exq.Ui do
       switches: [host: :string, port: :integer, namespace: :string, queues: :string, webport: :integer]
 
     webport = Keyword.get(opts, :webport, 4040)
-
+    opts = Keyword.put(opts, :host, to_char_list(Keyword.get(opts, :host, "127.0.0.1")))
     Exq.start(opts) 
     IO.puts "Started ExqUI on Port #{webport}"
     Plug.Adapters.Cowboy.http Exq.RouterPlug, [namespace: ""], port: webport
