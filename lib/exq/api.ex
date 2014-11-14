6c147ae5de2d315765ee4ff849fa221f88361c47
Justin McNally <justin@kohactive.com>
2014-11-13 22:31:22 -0600

    exq changes

---
+++ lib/exq/api.ex
@@ -0,0 +1,76 @@
+defmodule Exq.Api do
+  require Logger
+
+  def queues(pid) do
+    GenServer.call(pid, {:queues})
+  end
+
+  def busy(pid) do
+    GenServer.call(pid, {:busy})
+  end 
+
+  def stats(pid, key) do
+    GenServer.call(pid, {:stats, key})
+  end 
+
+  def stats(pid, key, date) do
+    GenServer.call(pid, {:stats, key, date})
+  end
+
+  def processes(pid) do
+    GenServer.call(pid, {:processes})
+  end
+
+  def jobs(pid) do
+    GenServer.call(pid, {:jobs})
+  end
+
+  def jobs(pid, queue) do
+    GenServer.call(pid, {:jobs, queue})
+  end
+
+  def failed(pid) do
+    GenServer.call(pid, {:failed})
+  end
+
+  def queue_size(pid) do
+    GenServer.call(pid, {:queue_size})
+  end
+
+  def queue_size(pid, queue) do
+    GenServer.call(pid, {:queue_size, queue})
+  end
+
+  def find_failed(pid, jid) do
+    GenServer.call(pid, {:find_failed, jid})
+  end
+
+  def find_job(pid, queue, jid) do
+    GenServer.call(pid, {:find_job, queue, jid})
+  end
+
+  def remove_queue(pid, queue) do
+    GenServer.call(pid, {:remove_queue, queue})
+  end
+
+  def remove_failed(pid, jid) do
+    GenServer.call(pid, {:remove_failed, jid})
+  end
+
+  def clear_failed(pid) do
+    GenServer.call(pid, {:clear_failed})
+  end
+
+  def clear_processes(pid) do
+    GenServer.call(pid, {:clear_processes})
+  end
+
+  def retry_failed(pid, jid) do
+    GenServer.call(pid, {:retry_failed, jid})
+  end
+
+  def realtime_stats(pid) do
+    GenServer.call(pid, {:realtime_stats})
+  end
+
+end
---
+++ lib/mix/tasks/exq.ui.ex
@@ -0,0 +1,27 @@
+defmodule Mix.Tasks.Exq.Ui do
+  use Mix.Task
+
+  @shortdoc "Starts the Exq UI Server"
+
+  def run(args) do
+    
+    
+    {opts, args, _} = OptionParser.parse args,
+      switches: [host: :string, port: :integer, namespace: :string, queues: :string, webport: :integer]
+
+    webport = Keyword.get(opts, :webport, 4040)
+    opts = Keyword.put(opts, :host, to_char_list(Keyword.get(opts, :host, "127.0.0.1")))
+    opts = Keyword.put(opts, :name, :exq_enq_ui)
+
+    # Start exq enqueuer supervisor
+
+    Exq.Enqueuer.Supervisor.start_link(opts)
+
+    IO.puts "Started ExqUI on Port #{webport}"
+
+    Plug.Adapters.Cowboy.http Exq.RouterPlug, [namespace: "", exqopts: opts], port: webport
+    
+    :timer.sleep(:infinity)
+  end
+
+end
