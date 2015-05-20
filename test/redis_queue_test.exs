e319913219e62315d0be616ed7ede7ac0675104d
Alex Kira <alex.kira@gmail.com>
2015-05-19 22:19:02 -0700

    Test for full_key

--- test/redis_queue_test.exs
+++ test/redis_queue_test.exs
@@ -8,22 +8,28 @@ defmodule Exq.RedisQueueTest do
     on_exit fn ->
       TestRedis.teardown
     end
-  end 
+  end
 
- 
+
   test "enqueue/dequeue single queue" do
     Exq.RedisQueue.enqueue(:testredis, "test", "default", "MyWorker", [])
     deq = Exq.RedisQueue.dequeue(:testredis, "test", "default")
-    assert deq != :none 
+    assert deq != :none
     assert Exq.RedisQueue.dequeue(:testredis, "test", "default") == :none
   end
-  
+
   test "enqueue/dequeue multi queue" do
     Exq.RedisQueue.enqueue(:testredis, "test", "default", "MyWorker", [])
     Exq.RedisQueue.enqueue(:testredis, "test", "myqueue", "MyWorker", [])
     assert Exq.RedisQueue.dequeue(:testredis, "test", ["default", "myqueue"]) != :none
     assert Exq.RedisQueue.dequeue(:testredis, "test", ["default", "myqueue"]) != :none
     assert Exq.RedisQueue.dequeue(:testredis, "test", ["default", "myqueue"]) == :none
+  end
+
+  test "full_key" do
+    assert Exq.RedisQueue.full_key("exq","k1") == "exq:k1"
+    assert Exq.RedisQueue.full_key("","k1") == "k1"
+    assert Exq.RedisQueue.full_key(nil,"k1") == "k1"
   end
 
   test "creates and returns a jid" do
