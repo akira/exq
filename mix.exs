2d064f2e95f082c1460ccfa9657ecffd9e499519
Alex Kira <alex.kira@gmail.com>
2015-08-02 13:48:17 -0700

    Bump hex version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exq.Mixfile do
 
   def project do
     [ app: :exq,
-      version: "0.1.4",
+      version: "0.2.0",
       elixir: "~> 1.0.0",
       elixirc_paths: ["lib", "web"],
       package: [
