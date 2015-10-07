97fe38edf03712e37b53cb2d8b1f2e1e57cb15f8
Andreas Franzén <andreas@devil.se>
2015-10-07 09:43:34 +0200

    Add redis_timeout config option
    Use redis timeout in GenServcer.call's
    
    Add timeout to :eredis.q calls
    
    Reset lib/exq.ex

--- lib/exq/redis/connection.ex
+++ lib/exq/redis/connection.ex
@@ -3,8 +3,8 @@ defmodule Exq.Redis.Connection do
   alias Exq.Support.Config
 
   def connection(opts \\ []) do
-    {host, port, database, password, reconnect_on_sleep} = info(opts)
-    :eredis.start_link(host, port, database, password, reconnect_on_sleep)
+    {host, port, database, password, reconnect_on_sleep, timeout} = info(opts)
+    :eredis.start_link(host, port, database, password, reconnect_on_sleep, timeout)
   end
 
   def info(opts \\ []) do
@@ -13,118 +13,122 @@ defmodule Exq.Redis.Connection do
     database = Keyword.get(opts, :database, Config.get(:database, 0))
     password = Keyword.get(opts, :password, Config.get(:password) || '')
     reconnect_on_sleep = Keyword.get(opts, :reconnect_on_sleep, Config.get(:reconnect_on_sleep, 100))
+    timeout = Keyword.get(opts, :redis_timeout, Config.get(:redis_timeout, 5000))
     if is_binary(host), do: host = String.to_char_list(host)
     if is_binary(password), do: password = String.to_char_list(password)
-    {host, port, database, password, reconnect_on_sleep}
+    {host, port, database, password, reconnect_on_sleep, timeout}
   end
 
   def flushdb!(redis) do
-    {:ok, res} = :eredis.q(redis, [:flushdb])
+    {:ok, res} = q(redis, [:flushdb])
     res
   end
 
   def decr!(redis, key) do
-    {:ok, count} = :eredis.q(redis, ["DECR", key])
+    {:ok, count} = q(redis, ["DECR", key])
     count
   end
 
   def incr!(redis, key) do
-    {:ok, count} = :eredis.q(redis, ["INCR", key])
+    {:ok, count} = q(redis, ["INCR", key])
     count
   end
 
   def get!(redis, key) do
-    {:ok, val} = :eredis.q(redis, ["GET", key])
+    {:ok, val} = q(redis, ["GET", key])
     val
   end
 
   def set!(redis, key, val \\ 0) do
-    :eredis.q(redis, ["SET", key, val])
+    q(redis, ["SET", key, val])
   end
 
   def del!(redis, key) do
-    :eredis.q(redis, ["DEL", key])
+    q(redis, ["DEL", key])
   end
 
   def expire!(redis, key, time \\ 10) do
-    :eredis.q(redis, ["EXPIRE", key, time])
+    q(redis, ["EXPIRE", key, time])
   end
 
   def llen!(redis, list) do
-    {:ok, len} = :eredis.q(redis, ["LLEN", list])
+    {:ok, len} = q(redis, ["LLEN", list])
     len
   end
 
   def keys!(redis, search \\ "*") do
-    {:ok, keys} = :eredis.q(redis, ["KEYS", search])
+    {:ok, keys} = q(redis, ["KEYS", search])
     keys
   end
 
   def scan!(redis, cursor, search, count) do
-    {:ok, keys} = :eredis.q(redis, ["SCAN", cursor, "MATCH", search, "COUNT", count])
+    {:ok, keys} = q(redis, ["SCAN", cursor, "MATCH", search, "COUNT", count])
     keys
   end
 
   def scard!(redis, set) do
-    {:ok, count} = :eredis.q(redis, ["SCARD", set])
+    {:ok, count} = q(redis, ["SCARD", set])
     count
   end
 
   def smembers!(redis, set) do
-    {:ok, members} = :eredis.q(redis, ["SMEMBERS", set])
+    {:ok, members} = q(redis, ["SMEMBERS", set])
     members
   end
 
   def sadd!(redis, set, member) do
-    {:ok, res} = :eredis.q(redis, ["SADD", set, member])
+    {:ok, res} = q(redis, ["SADD", set, member])
     res
   end
 
   def srem!(redis, set, member) do
-    {:ok, res} = :eredis.q(redis, ["SREM", set, member])
+    {:ok, res} = q(redis, ["SREM", set, member])
     res
   end
 
   def lrange!(redis, list, range_start \\ "0", range_end \\ "-1") do
-    {:ok, items} = :eredis.q(redis, ["LRANGE", list, range_start, range_end])
+    {:ok, items} = q(redis, ["LRANGE", list, range_start, range_end])
     items
   end
 
   def lrem!(redis, list, value, count \\ 1) do
-    {:ok, res} = :eredis.q(redis, ["LREM", list, count, value])
+    {:ok, res} = q(redis, ["LREM", list, count, value])
     res
   end
 
   def rpush!(redis, key, value) do
-    {:ok, res} = :eredis.q(redis, ["RPUSH", key, value])
+    {:ok, res} = q(redis, ["RPUSH", key, value])
     res
   end
 
   def lpop!(redis, key) do
-    case :eredis.q(redis, ["LPOP", key]) do
+    case q(redis, ["LPOP", key]) do
       {:ok, :undefined} -> :none
       {:ok, value} -> value
     end
   end
 
   def zadd!(redis, set, score, member) do
-    {:ok, res} = :eredis.q(redis, ["ZADD", set, score, member])
+    {:ok, res} = q(redis, ["ZADD", set, score, member])
     res
   end
 
   def zcard!(redis, set) do
-    {:ok, count} = :eredis.q(redis, ["ZCARD", set])
+    {:ok, count} = q(redis, ["ZCARD", set])
     count
   end
 
   def zrangebyscore!(redis, set, min \\ "0", max \\ "+inf") do
-    {:ok, items} = :eredis.q(redis, ["ZRANGEBYSCORE", set, min, max])
+    {:ok, items} = q(redis, ["ZRANGEBYSCORE", set, min, max])
     items
   end
 
   def zrem!(redis, set, member) do
-    {:ok, res} = :eredis.q(redis, ["ZREM", set, member])
+    {:ok, res} = q(redis, ["ZREM", set, member])
     res
   end
 
+  defp q(redis, command) do
+    :eredis.q(redis, command, Config.get(:redis_timeout, 5000))
+  end
 end
