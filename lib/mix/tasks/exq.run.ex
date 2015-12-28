efa9429959126a6de8801934b306da02ed1241c2
Alex Kira <alex.kira@gmail.com>
2015-12-27 16:53:37 -0800

    Use mix run --no-halt for exq.run task

--- lib/mix/tasks/exq.run.ex
+++ lib/mix/tasks/exq.run.ex
@@ -4,31 +4,7 @@ defmodule Mix.Tasks.Exq.Run do
   @shortdoc "Starts the Exq worker"
 
   def run(args) do
-    {opts, args, _} = OptionParser.parse args,
-      switches: [host: :string, port: :integer, namespace: :string, queues: :string, webport: :integer]
-    app = parse_app(args)
-    if app do
-      app.start(nil)
-    end
-    opts = Keyword.put(opts, :host, to_char_list(Keyword.get(opts, :host, "127.0.0.1")))
-    opts = Keyword.put(opts, :port, Keyword.get(opts, :port, 6379))
-    opts = Keyword.put(opts, :namespace, to_char_list(Keyword.get(opts, :namespace, "exq")))
-    {:ok, _pid} = Exq.start(opts)
-    IO.puts "Started Exq with redis options: Host: #{opts[:host]}, Port: #{opts[:port]}, Namespace: #{opts[:namespace]}"
-    :timer.sleep(:infinity)
+    IO.puts("Starting Exq...")
+    Mix.Task.run "run", ["--no-halt"] ++ args
   end
-
-  def parse_app([h|t]) when is_binary(h) and h != "" do
-    {Module.concat([h]), t}
-  end
-
-  def parse_app([h|t]) when is_atom(h) and h != :"" do
-    {h, t}
-  end
-
-  def parse_app(_) do
-    nil
-    #raise Mix.Error, message: "invalid arguments, expected an applicaiton as first argument"
-  end
-
 end
