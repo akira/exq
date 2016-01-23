37e524602bbed5e3cff922784765a1942dd58bcc
Alex Kira <alex.kira@gmail.com>
2016-01-22 22:06:08 -0800

    Bump to v0.6.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exq.Mixfile do
 
   def project do
     [ app: :exq,
-      version: "0.6.2",
+      version: "0.6.3",
       elixir: "~> 1.0.0 or ~> 1.1.1 or ~> 1.2",
       elixirc_paths: ["lib"],
       package: [
