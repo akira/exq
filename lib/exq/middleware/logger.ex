b73d9b085fcc2ade5cb536c86c448bc2f7c8630e
edmz <edmz@users.noreply.github.com>
2016-02-03 17:03:38 -0600

    log time in ms, µs just as phoenix/plug does
    Actually, it borrows the code from plug.

--- lib/exq/middleware/logger.ex
+++ lib/exq/middleware/logger.ex
@@ -12,13 +12,13 @@ defmodule Exq.Middleware.Logger do
   end
 
   def after_processed_work(pipeline) do
-    Logger.info("#{log_context(pipeline)} done: #{delta(pipeline)} sec")
+    Logger.info("#{log_context(pipeline)} done: #{formatted_diff(delta(pipeline))} sec")
     pipeline
   end
 
   def after_failed_work(pipeline) do
     Logger.info(to_string(pipeline.assigns.error_message))
-    Logger.info("#{log_context(pipeline)} fail: #{delta(pipeline)} sec")
+    Logger.info("#{log_context(pipeline)} fail: #{formatted_diff(delta(pipeline))} sec")
     pipeline
   end
 
@@ -30,4 +30,7 @@ defmodule Exq.Middleware.Logger do
   defp log_context(%Pipeline{assigns: assigns}) do
     "#{assigns.worker_module}[#{assigns.job.jid}]"
   end
+  
+  defp formatted_diff(diff) when diff > 1000, do: [diff |> div(1000) |> Integer.to_string, "ms"]
+  defp formatted_diff(diff), do: [diff |> Integer.to_string, "µs"]  
 end
