1a8c9a2fcd857c17316894aec9747e44fe9802b5
Alex Kira <alex.kira@gmail.com>
2017-03-08 22:20:57 -0800

    Bump mix to 0.8.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exq.Mixfile do
 
   def project do
     [ app: :exq,
-      version: "0.8.3",
+      version: "0.8.4",
       elixir: "~> 1.3",
       elixirc_paths: ["lib"],
       package: [
