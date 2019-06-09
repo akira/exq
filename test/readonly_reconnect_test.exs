ca20006f467a4ec49e5fa23f446050683b4be771
Bharanee Rathna <bharanee@blake.com.au>
2019-06-10 09:47:55 +1000

    Add additional test for pipeline.

--- test/readonly_reconnect_test.exs
+++ test/readonly_reconnect_test.exs
@@ -10,12 +10,22 @@ defmodule ReadonlyReconnectTest do
     :ok
   end
 
-  test "test disconnect on errors against read-only redis" do
+  test "test disconnect on read-only errors with single command" do
     Process.flag(:trap_exit, true)
     {:ok, redis} = Redix.start_link(host: "127.0.0.1", port: 6556)
     Process.register(redis, :testredis)
 
     Exq.Redis.Connection.q(:testredis, ["SET", "key", "value"])
+    assert_received({:EXIT, pid, :killed})
+    assert redis == pid
+  end
+
+  test "test disconnect on read-only errors with command pipeline" do
+    Process.flag(:trap_exit, true)
+    {:ok, redis} = Redix.start_link(host: "127.0.0.1", port: 6556)
+    Process.register(redis, :testredis)
+
+    Exq.Redis.Connection.qp(:testredis, [["GET", "key"], ["SET", "key", "value"]])
     assert_received({:EXIT, pid, :killed})
     assert redis == pid
   end
